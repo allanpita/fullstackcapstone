12f012dd708897dc4590ac58ecfdb21a2ceef6f5
apitagit config --global user.name apitagit config --global user.name apita <allanpita.j@gmail.com>
2019-02-24 13:23:59 -0500

    fixes mongoconfig var

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ app.use(bodyParser.json())
 
 const { DATABASE_URL, PORT } = require('./mongoconfig');
 
-//mongoose promise connection
+//mongoose promise connection (for testing locally)
+/*
 const mongoose = require('mongoose');
 mongoose.Promise=global.Promise;
 mongoose.connect('mongodb://localhost:27017/student')
   .then(() =>  console.log('connected to mongo succesfully'))
   .catch((err) => console.error(err));
-
+*/
 //Morgan Loger
 app.use(morgan("common"));
 app.use('/', require('./routes/routes'))
@@ -23,32 +24,46 @@ app.use('/', require('./routes/routes'))
 //start server / connections
 let server;
 
-function startServer() {
-  const port = process.env.PORT || 8080;
+// this function connects to our database, then starts the server
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    })
-    .on('error', err => {
-      reject(err);
-    });
-  });
-}
-function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log("Closing server");
-    server.close(err => {
+    mongoose.connect(databaseUrl, err => {
+      console.log(`mongoDB is located at: ${databaseUrl}`);
       if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
+        return reject(err);
       }
-      resolve();
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
     });
   });
 }
 
-if (require.main === module) {startServer().catch(err => console.error(err));};
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
 
-module.exports = {app, startServer, closeServer};
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { runServer, app, closeServer };
