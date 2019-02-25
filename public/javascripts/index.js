0bbed2adc6426d61c6f88dc8fbfa43dd4577110a
allanpita <44935398+allanpita@users.noreply.github.com>
2019-02-25 15:44:03 +0430

    made some edits on the client side JS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,26 +5,24 @@ id = $('#studentID').val();
 let ln = $('#lastName').val();
 let fn= $('#firstName').val();
 console.log(`SID: ${id} fn: ${fn} ln: ${ln}`)
-$('#results').empty();
-$('#results').removeClass('hideSection')
+$('#results').removeClass('hideContent');
+$('#results ul').empty();
 queryHandle(id);
 }); 
 
-(#resultsArea').removeClass('hideSection')
-//$('#results').empty();
-//let ipAddress = $('#ip').val(); 
-//queryHandle(ipAddress);
-
-//end of Submit function
 
 function queryHandle(id){
-const url = `http://localhost:8080/api/${id}`;
-
+const url = `https://peaceful-thicket-95451.herokuapp.com/api/${id}`;
 fetch(url)
 .then(response => response.json())  //transform data from GET into JSON format. 
-.then(jsonData =>{//pivot to data. 
-Object.entries(jsonData).forEach(item => { //iterate tru each object entrie and adds to the list of the html page. 
-$('#results').append(`<li><strong>${item[0]}</strong>:  <font color="red">${item[1]}</font></li>`); // add data object to the HTML OL. 
-}); //Object.entries section 
-}); //Data pivot section
-}; //query handle function
+.then(jsonData =>{                  //pivot to data.
+Object.entries(jsonData).forEach(item =>{
+  let mainString = `${item[0]['firstname']}`;
+  console.log(mainString)
+}) 
+Object.entries(jsonData.curriculum).forEach(sitem => {
+  console.log(sitem[1].class); 
+//$('#results ul').append(`<li>${item[1]}</li>`);
+})
+})
+}
