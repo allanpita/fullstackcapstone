ffcce6e45806243246e444992a98374651a52c1f
allanpita <44935398+allanpita@users.noreply.github.com>
2019-03-02 15:47:15 +0430

    Implemented Navigation

---
+++ public/javascripts/htmltemplates.js
@@ -0,0 +1,23 @@
+let studentInfo = 
+          `<input type="text" id="studentID" name="studentID" placeholder="Student ID" required>
+          <input type="text" id="firstName" name="firstName" placeholder="First Name" required>
+          <input type="text" id="lastName" name="lastName" placeholder="Last Name" required>
+          <input type="number" id="age" name="age" placeholder="Age" required>`
+
+
+let studentCur = 
+          `<legend>Math:</legend>
+          <input type="number" id="mathGrade" name="mathGrade" placeholder="GPA" required>
+          <input type="number" id="mathAbs" name="mathAbs" placeholder="absences">
+          <legend>English:</legend>
+          <input type="number" id="englishGrade" name="englishGrade" placeholder="GPA" required>
+          <input type="number" id="englishAbs" name="englishAbs" placeholder="absences">
+          <legend>History:</legend>
+          <input type="number" id="historyGrade" name="historyGrade" placeholder="GPA" required>
+          <input type="number" id="historyAbs" name="historyAbs" placeholder="absences">
+          <legend>Science</legend>
+          <input type="number" id="scienceGrade" name="scienceGrade" placeholder="GPA" required>
+          <input type="number" id="scienceAbs" name="scienceAbs" placeholder="absences">
+          <legend>Elective</legend>
+          <input type="number" id="electiveGrade" name="electiveGrade" placeholder="GPA" required>
+          <input type="number" id="electiveAbs" name="electiveAbs" placeholder="absences">`
